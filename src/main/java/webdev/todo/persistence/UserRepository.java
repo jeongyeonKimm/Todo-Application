863d379794199193a7e14332b044f8003fd0f17d
jeongyeonKimm <wjddus9132@gmail.com>
2023-01-07 21:16:50 +0900

    feat: User repository 구현

---
+++ src/main/java/webdev/todo/persistence/UserRepository.java
@@ -0,0 +1,12 @@
+package webdev.todo.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import webdev.todo.model.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    UserEntity findByEmail(String email);
+    Boolean existsByEmail(String email);
+    UserEntity findByEmailAndPassword(String email, String password);
+}
